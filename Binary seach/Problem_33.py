e3d9f02d0762873355f020945285976d3e635155
EC2 Default User <ec2-user@ip-172-31-22-173.ap-southeast-1.compute.internal>
2022-07-14 17:06:30 +0000

    No.33

---
+++ Binary seach/Problem_33.py
@@ -0,0 +1,25 @@
+# binary search
+# No.33 https://leetcode.com/problems/search-in-rotated-sorted-array/
+# Just solve this problem with binary search and add a judgement before choosing right or left part of next loop.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = int((left + right) / 2)
+            print("%s %s %s" % (left, mid, right))
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < nums[left]:
+                if nums[right] >= target and nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if nums[left] <= target and nums[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
+
+
